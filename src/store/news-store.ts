874d82c62d00721679b5a89a47b73e578af15b51
64576 <64576@etu.he2b.be>
2025-04-02 10:39:12 +0200

    test d'un fichier ts en plus

--- src/store/news-store.ts
+++ src/store/news-store.ts
@@ -1,155 +1,60 @@
 import { StateCreator, create } from "zustand";
-import { AppState } from ".";
 import { News, NewsType } from "../model/news";
+import news from "./news.ts";  // Tu importes tes données ici
 
 export interface NewsStore {
-	news: News[];
-	setNews: (news: News[]) => void;
-	allTags: string[];
-	tagFilters: string[];
-	setTagFilters: (tagFilters: string[]) => void;
-	toggleTagFilter: (tagFilter: string) => void;
-	getFilteredNews: () => News[];
-	tagColors: Record<NewsType | string, string>;
-	getTagsColor: (tag: (NewsType | string)[]) => string;
+  news: News[];
+  setNews: (news: News[]) => void;
+  allTags: string[];
+  tagFilters: string[];
+  setTagFilters: (tagFilters: string[]) => void;
+  toggleTagFilter: (tagFilter: string) => void;
+  getFilteredNews: () => News[];
+  tagColors: Record<NewsType | string, string>;
+  getTagsColor: (tag: (NewsType | string)[]) => string;
 }
 
-
-export const createNewsStore: StateCreator<AppState, [], [], NewsStore> = (set, get) => ({
-	news: defaultNews,
-	setNews: news => set({ news }),
-	allTags: defaultAllTags,
-	tagFilters: defaultTagFilters,
-	setTagFilters: tagFilters => set({ tagFilters }),
-	toggleTagFilter: tagFilter => {
-		const { tagFilters } = get();
-		set({
-			tagFilters: tagFilters.includes(tagFilter)
-				? tagFilters.filter((t) => t !== tagFilter)
-				: [...tagFilters, tagFilter]
-		});
-	},
-	getFilteredNews: () => {
-		const { news, tagFilters } = get();
-		return news
-			.filter((item) => !tagFilters.length || item.tags.some((tag) => tagFilters.includes(tag)))
-			.sort((a, b) => b.date.getTime() - a.date.getTime());
-	},
-	tagColors: defaultTagColors,
-	getTagsColor: tags => {
-		const { tagColors } = get();
-		return tagColors[tags.find(tag => tag in tagColors) ?? NewsType.OTHER];
-	},
-});
-
-
-const defaultNews: News[] = [
-	{
-		type: "absence",
-		id: "1",
-		date: new Date("2021-09-01"),
-		tags: [NewsType.ABSENCE],
-		teacherName: "Monsieur ABE",
-		teacherMonogram: "ABE",
-		teacherGender: "M",
-		className: "SAR-MAT2",
-		classCode: "SAR-MAT2",
-		classStartTime: new Date("2021-09-01T08:00:00"),
-		classEndTime: new Date("2021-09-01T10:00:00"),
-		groupCode: "SAR-MAT2",
-		title: "Absence de Monsieur ABE",
-		description: "Monsieur ABE sera absent ce jeudi 1er septembre. Le cours de SAR-MAT2 de 8h est donc annulé et reporté à une date ultérieure.",
-	},
-	{
-		type: "ce",
-		id: "2",
-		date: new Date("2021-09-01"),
-		tags: [NewsType.CE],
-		title: "Candidature élections CE",
-		description: "Par ce mail le Conseil Étudiant de la HE2B annonce l'ouverture de la période des élections pour la période académique 2025-2026. Vous trouverez dans ce mail notre Règlement Électoral, le calendrier électoral ainsi qu'une copie de la liste des électeur·trice·s de la HE2B.",
-	},
-	{
-		type: "sar",
-		id: "3",
-		date: new Date("2021-09-02"),
-		tags: [NewsType.SAR],
-		title: "Réorientation HE2B",
-		description: "Après avoir terminé votre session d'examens, il est possible que vous ayez des doutes sur votre orientation actuelle. Peut-être ne savez-vous pas quoi faire ?",
-	},
-	{
-		type: "absence",
-		id: "4",
-		date: new Date("2021-02-06"),
-		tags: [NewsType.ABSENCE, NewsType.SAR, "esi", "mat2"],
-		title: "Absence XYZ",
-		teacherMonogram: "xyz",
-		description: "M. XYZ sera absent ce jeudi 6 février. Le cours de SAR-MAT2 de 16h est donc annulé et reporté à une date ultérieure.",
-	},
-	{
-		type: "marks",
-		id: "5",
-		date: new Date("2021-09-03"),
-		tags: [NewsType.MARKS],
-		className: "SAR-MAT2",
-		classCode: "SAR-MAT2",
-		title: "Résultats de la session",
-		description: "Les résultats de la session de janvier 2021 sont disponibles sur le site de la HE2B. Vous pouvez consulter vos résultats en vous connectant à votre compte étudiant.",
-	},
-	{
-		type: "communication",
-		id: "6",
-		date: new Date("2021-09-03"),
-		tags: [NewsType.COMMUNICATION],
-		title: "communication administratif",
-	},
-	{
-		type: "other",
-		id: "7",
-		date: new Date("2021-09-04"),
-		tags: [NewsType.OTHER],
-		title: "Autres informations",
-	},
-	{
-		type: "sport",
-		id: "8",
-		date: new Date("2022-09-04"),
-		tags: [NewsType.SPORT],
-		title: "Journée sportive",
-	},
-	{
-		type: "absence",
-		id: "9",
-		date: new Date("2023-05-24"),
-		tags: [NewsType.ABSENCE],
-		teacherName: "Monsieur ABE",
-		teacherMonogram: "ABE",
-		teacherGender: "M",
-		className: "SAR-MAT2",
-		classCode: "SAR-MAT2",
-		classStartTime: new Date("2023-05-24T08:00:00"),
-		classEndTime: new Date("2023-05-24T10:00:00"),
-		groupCode: "SAR-MAT2",
-		title: "Absence de Monsieur ABE",
-		description: "Monsieur ABE sera absent ce jeudi 24 mai. Le cours de SAR-MAT2 de 8h est donc annulé et reporté à une date ultérieure.",
-	},
-];
-
-const defaultAllTags = [...new Set(defaultNews.flatMap((item) => item.tags)).values()]
-	.sort();
+const defaultAllTags = [...new Set(news.flatMap((item) => item.tags)).values()]
+  .sort();
 
 const defaultTagFilters: string[] = [];
 
 const defaultTagColors: Record<NewsType | string, string> = {
-	[NewsType.ABSENCE]: "#FF4D4F",
-	[NewsType.CE]: "#40A9FF",
-	[NewsType.SAR]: "#7E4B8B",
-	[NewsType.MARKS]: "#FFD700",
-	[NewsType.COMMUNICATION]: "#28A745",
-	[NewsType.OTHER]: "#8E8E8E",
-	[NewsType.SPORT]: "#FF8C00",
+  [NewsType.ABSENCE]: "#FF4D4F",
+  [NewsType.CE]: "#40A9FF",
+  [NewsType.SAR]: "#7E4B8B",
+  [NewsType.MARKS]: "#FFD700",
+  [NewsType.COMMUNICATION]: "#28A745",
+  [NewsType.OTHER]: "#8E8E8E",
+  [NewsType.SPORT]: "#FF8C00",
 };
 
-export const useAppStore = create((set) => ({
-    selectedTags: [],
-    setSelectedTags: (tags: any) => set({ selectedTags: tags }),
-}));
+export const createNewsStore: StateCreator<NewsStore> = (set, get) => ({
+  news: news,  // Initialiser l'état avec les données importées
+  setNews: (news) => set({ news }),
+  allTags: defaultAllTags,
+  tagFilters: defaultTagFilters,
+  setTagFilters: (tagFilters) => set({ tagFilters }),
+  toggleTagFilter: (tagFilter) => {
+    const { tagFilters } = get();
+    set({
+      tagFilters: tagFilters.includes(tagFilter)
+        ? tagFilters.filter((t) => t !== tagFilter)
+        : [...tagFilters, tagFilter],
+    });
+  },
+  getFilteredNews: () => {
+    const { news, tagFilters } = get();
+    return news
+      .filter((item) => !tagFilters.length || item.tags.some((tag) => tagFilters.includes(tag)))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+  },
+  tagColors: defaultTagColors,
+  getTagsColor: (tags) => {
+    const { tagColors } = get();
+    return tagColors[tags.find((tag) => tag in tagColors) ?? NewsType.OTHER];
+  },
+});
+
+// Utilisation de `create` pour créer le store
+export const useNewsStore = create(createNewsStore);
